4056b2e0d00c71f4a40464a0fe123b5e8df329c0
cmtn-86 <58218011+cmtn-86@users.noreply.github.com>
2020-11-09 20:45:17 +0100

    Gui für einLogin

---
+++ src/einLogin.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class einLogin extends  JFrame{
+    private JPanel einLoginPanel;
+    private JTextField einGabeBenutzerNameTextField;
+    private JTextField textField1;
+    private JButton anmeldenButton;
+
+    public einLogin()
+    {
+        add(einLoginPanel);
+        setSize(400,200);
+        setTitle("Kunden Datenbank");
+    }
+
+
+}
---
+++ src/mainClass.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class mainClass {
+
+    public static void main(String[] args){
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        einLogin eL =new einLogin();
+        eL.setLocation(700,300);
+        eL.setVisible(true);
+
+    }
+
+
+}
